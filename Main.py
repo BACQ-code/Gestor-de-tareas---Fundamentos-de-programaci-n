eebdb6ac2126787a0d7c4c8851ed8938c7b2423c
ismael-suquillo <ifsuquillo@puce.edu.ec>
2026-08-14 12:12:22 -0500

    Se agrega la función del menú principal nuevamente

--- Main.py
+++ Main.py
@@ -1,14 +1,29 @@
+tareas = []
+
+def mostrar_menu():
+    print("\nGESTOR DE TAREAS")
+    print("1. Agregar tarea")
+    print("2. Listar tareas")
+    print("3. Mostrar progreso")
+    print("4. Salir")
+
+def agregar_tarea():
+    nombre = input("Tarea: ")
+    tarea = {
+        "nombre": nombre,
+        "completada": False
+    }
+    tareas.append(tarea)
+
 
     total = len(tareas)
     if total == 0:
         print("Sin tareas")
         return
-
     completadas = 0
     for tarea in tareas:
         if tarea["completada"]:
             completadas += 1
-
     porcentaje = (
         completadas * 100 / total
     )
